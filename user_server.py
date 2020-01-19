4e896603179cc1806ac90398178656f2e03eb513
Adnan Munawar <adnan.munawar@live.com>
2020-01-18 21:34:06 -0500

    Created class for user_server

--- user_server.py
+++ user_server.py
@@ -1,16 +1,32 @@
 import socket
 import time
 
-# Globals
-server_addr = ''
-server_port = 3004
 
-def set_vtx_pos(idx, x, y, z):
-    packet = str(idx) + ',' + str(x) + ',' + str(y) + ',' + str(z)
-    client.send(packet.encode())
+class UserServer:
+    def __init__(self):
+        # Globals
+        self.server_addr = ''
+        self.server_port = 3004
+        self.server = None
+        self.client = None
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((server_addr, server_port))
-server.listen(1)
-client, client_addr = server.accept()
-print('Connected To: ', client_addr)
+    def create_server(self, addr=None, port=None):
+        if addr is not None:
+            self.server_addr = addr
+
+        if port is not None:
+            self.server_port = port
+
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((self.server_addr, self.server_port))
+        self.server.listen(1)
+        print('Waiting For Connection on: ', self.server_addr, ':', self.server_port)
+        self.client, client_addr = self.server.accept()
+        print('Connected To: ', client_addr)
+
+    def set_vtx_pos(self, idx, x, y, z):
+        packet = str(idx) + ',' + str(x) + ',' + str(y) + ',' + str(z)
+        self.client.send(packet.encode())
+
+    def get_vtx_pos(self, idx):
+        pass
